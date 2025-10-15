290122c4d117fa7c0a526808200882a13869b5cb
exsarorrayzer <theleapy@gmail.com>
2025-10-15 18:03:24 +0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from .core.obfuscator import Obfuscator
+from .core.transformers import (
+    VariableRenamer,
+    StringObfuscator,
+    ImportObfuscator,
+    CommentRemover,
+    MarshallTransformer,
+    ZlibTransformer
+)
+from .encryption import MarshallEncoder, ZlibCompressor
+
+__version__ = "1.1.0"
+__author__ = "exsarorrayzer"
+
+__all__ = [
+    'Obfuscator',
+    'VariableRenamer', 
+    'StringObfuscator',
+    'ImportObfuscator',
+    'CommentRemover',
+    'MarshallTransformer',
+    'ZlibTransformer',
+    'MarshallEncoder',
+    'ZlibCompressor'
+]
