97adae4af85ac039123aff8df13c6b8e93eff657
exsarorrayzer <theleapy@gmail.com>
2025-10-15 18:45:48 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'insider=.insider.cli.main:main',
+            'insider=insider.cli.main:main',  # ✅ DOĞRU
         ],
     },
 )
